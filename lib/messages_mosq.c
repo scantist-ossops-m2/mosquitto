b3a0b712fdf3ec01759c739d513d4346c4c34541
turly221 <feifan@scntist.com>
2024-12-09 06:02:07 +0000

    commit patch 20549074

--- lib/messages_mosq.c
+++ lib/messages_mosq.c
@@ -178,7 +178,7 @@ void _mosquitto_messages_reconnect_reset(struct mosquitto *mosq)
 			* whatever the client has got. */
 		}
 		prev = message;
-		message = message->next;
+		if(message) message = message->next;
 	}
 	mosq->in_messages_last = prev;
 	pthread_mutex_unlock(&mosq->in_message_mutex);
